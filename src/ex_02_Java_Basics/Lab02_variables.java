66526f4d1947601634379bd3d990548d814dd749
Sharma-deeksha <pandey.deeksha19@gmail.com>
2025-05-11 10:09:50 +0530

    11-05-2025

---
+++ src/ex_02_Java_Basics/Lab02_variables.java
@@ -0,0 +1,13 @@
+package ex_02_Java_Basics;
+
+public class Lab02_variables {
+    public static void main(String[] args) {
+        int age = 76;
+        age = 76+1 ;
+        age=78;
+        System.out.println(age);
+    }
+
+
+
+}
