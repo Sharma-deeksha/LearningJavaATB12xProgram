223927dbb23a33e35fc88f594a4d7bc5e11941e8
Sharma-deeksha <pandey.deeksha19@gmail.com>
2025-05-18 11:44:30 +0530

    11-05-2025

---
+++ src/ex_07_Increment_Decrement_Op/Lab068_POST_ID.java
@@ -0,0 +1,9 @@
+package ex_07_Increment_Decrement_Op;
+
+public class Lab068_POST_ID {
+    public static void main(String[] args) {
+        int a_post = 10;
+        System.out.println(a_post++);
+        System.out.println(a_post);
+    }
+}
