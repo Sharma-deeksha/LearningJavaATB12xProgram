23787cb5405f719ad46fa2c60b7cfea34350bf94
Sharma-deeksha <pandey.deeksha19@gmail.com>
2025-05-18 11:44:21 +0530

    11-05-2025

--- src/tasks/Task3_Max_in_3_Numbers.java
+++ src/tasks/Task3_Max_in_3_Numbers.java
@@ -3,8 +3,10 @@ package tasks;
 public class Task3_Max_in_3_Numbers {
     public static void main(String[] args) {
         int a=10;
-        int b=555;
-        int c=3;
-        System.out.println("Biggest number is -> " + (c>(a>b?a:b)?c:(a>b?a:b)));
+        int b=50;
+        int c=300;
+        System.out.print("Biggest Number is -> ");
+        System.out.println(a>b ? (a>c?a:c) : (b>c?b:c));
+
     }
 }
--- src/tasks/Task4_Minor_Sn_Adult.java
+++ src/tasks/Task4_Minor_Sn_Adult.java
@@ -2,7 +2,7 @@ package tasks;
 
 import java.util.Scanner;
 
-public class Task_4_Minor_Sn_Adult {
+public class Task4_Minor_Sn_Adult {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter age");
