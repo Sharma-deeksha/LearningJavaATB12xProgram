995bed499e86b76b425fce0e56be7d0e3fd40da2
Sharma-deeksha <pandey.deeksha19@gmail.com>
2025-06-22 09:58:54 +0530

    11-05-2025

---
+++ src/ex_25_Object/Lab229_objectClass.java
@@ -0,0 +1,9 @@
+package ex_25_Object;
+
+public class Lab229_objectClass {
+    public static void main(String[] args) throws InterruptedException {
+        Object o=new Object(); //Object is super class of all the classes
+        o.getClass();
+        o.wait(1500);
+    }
+}
--- src/tasks/Task24_ForLoopContinue.java
+++ src/tasks/Task24_ForLoopContinue.java
@@ -2,13 +2,10 @@ package tasks;
 
 public class Task24_ForLoopContinue {
     public static void main(String[] args) {
-       /* for (int i = 1; i <= 3; i++) {
+        for (int i = 1; i <= 3; i++) {
             if (i == 2) continue;
             System.out.print(i);
         }
-        int sum = 0; for (int i = 1; i <= 4; i++) { sum += i; }
-        System.out.println(sum);*/
 
-        for (int i = 0; i < 2; i++) { for (int j = 0; j < 2; j++) { System.out.print(i + j); } }
     }
 }
